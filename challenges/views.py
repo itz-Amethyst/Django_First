ce3fb275fda90ac0d456d283b96b6f91e6866d8c
itz-amethyst <pransermi@gmail.com>
2023-08-12 22:28:32 +0330

    feat: added days-of-week index

--- challenges/views.py
+++ challenges/views.py
@@ -13,13 +13,23 @@ days = {
 	"friday": "Today is Friday",
 }
 
+def days_list(request):
+	days_list = list(days.keys())
+	list_items = ""
 
+	for day in days_list:
+		url_path = reverse('days-of-week', args = [day])
+		list_items += f'<li> <a href="{url_path}"> {day} </a> </li> \n'
 
-def index(request):
-	return HttpResponse("this is a response from index")
+	content = f'<ul>\n {list_items} \n</ul>'
 
-def sunday(request):
-	return HttpResponse("this is a response from sunday")
+	return HttpResponse(content)
+
+# def index(request):
+# 	return HttpResponse("this is a response from index")
+
+# def sunday(request):
+# 	return HttpResponse("this is a response from sunday")
 
 def dynamic_days_by_number(request, day):
 	days_name = list(days.keys())
@@ -33,5 +43,6 @@ def dynamic_days_by_number(request, day):
 def dynamic_days(request , day):
 	day_data = days.get(day.lower())
 	if day_data is not None:
-		return HttpResponse(f"The data i found is : {day_data}")
+		response = f'The data i found is : <h1 style="color:red"> {day_data}</h1>'
+		return HttpResponse(response)
 	return HttpResponseNotFound("day does not exist")
