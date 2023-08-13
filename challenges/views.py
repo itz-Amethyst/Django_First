1c850746c42dbb4fcbc7fcbf0ab3f24ea13b9b54
itz-amethyst <pransermi@gmail.com>
2023-08-13 10:08:13 +0330

    feat: Added DTL

--- challenges/views.py
+++ challenges/views.py
@@ -44,6 +44,12 @@ def dynamic_days_by_number(request, day):
 def dynamic_days(request , day):
 	day_data = days.get(day.lower())
 	if day_data is not None:
-		response = render_to_string('challenges/index.html')
-		return HttpResponse(response)
+		data = {
+			"Rango" : day_data
+		}
+		#DTL -> Django Template Language
+
+		return render(request, 'challenges/index.html' , data)
+		# response = render_to_string('challenges/index.html')
+		# return HttpResponse(response)
 	return HttpResponseNotFound("day does not exist")
