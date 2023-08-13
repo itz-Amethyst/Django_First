fa2a7243a992f2759f4c9be00169ce3666bbd352
itz-amethyst <pransermi@gmail.com>
2023-08-13 12:05:47 +0330

    feat: Configured index page to DTL

--- challenges/views.py
+++ challenges/views.py
@@ -16,21 +16,10 @@ days = {
 
 def days_list(request):
 	days_list = list(days.keys())
-	list_items = ""
-
-	for day in days_list:
-		url_path = reverse('days-of-week', args = [day])
-		list_items += f'<li> <a href="{url_path}"> {day} </a> </li> \n'
-
-	content = f'<ul>\n {list_items} \n</ul>'
-
-	return HttpResponse(content)
-
-# def index(request):
-# 	return HttpResponse("this is a response from index")
-
-# def sunday(request):
-# 	return HttpResponse("this is a response from sunday")
+	context = {
+		'days': days_list
+	}
+	return render(request, "challenges/index.html", context)
 
 def dynamic_days_by_number(request, day):
 	days_name = list(days.keys())
@@ -41,15 +30,16 @@ def dynamic_days_by_number(request, day):
 	return HttpResponseRedirect(redirect_url)
 
 
-def dynamic_days(request , day):
+def dynamic_days(request, day):
 	day_data = days.get(day.lower())
 	if day_data is not None:
 		data = {
-			"Rango" : day_data
+			"Rango": day_data,
+			"Day": day
 		}
 		#DTL -> Django Template Language
 
-		return render(request, 'challenges/index.html' , data)
+		return render(request, 'challenges/show-item.html', data)
 		# response = render_to_string('challenges/index.html')
 		# return HttpResponse(response)
 	return HttpResponseNotFound("day does not exist")
